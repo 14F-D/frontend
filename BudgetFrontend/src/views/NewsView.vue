ce6ab366bb4d39b45d1065ecc67c5e21db4edf02
HalaszPeter1 <halasz.peter1@students.jedlik.eu>
2023-04-27 12:05:56 +0200

    news layout added

--- BudgetFrontend/src/views/NewsView.vue
+++ BudgetFrontend/src/views/NewsView.vue
@@ -1,6 +1,93 @@
 <template>
-    <div>
-
+    <div class="row mt-2">
+        <div class="col"></div>
+        <div class="col-8 col-lg-8 col-md-10 col-sm-10">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod praesentium esse
+                        soluta expedita recusandae dolor maxime, ab repudiandae sint quo similique quisquam! Culpa
+                        voluptatibus sed aperiam! Accusantium eos reiciendis perspiciatis!</p>
+                </div>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+    <div class="row mt-2">
+        <div class="col"></div>
+        <div class="col-8 col-lg-8 col-md-10 col-sm-10">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod praesentium esse
+                        soluta expedita recusandae dolor maxime, ab repudiandae sint quo similique quisquam! Culpa
+                        voluptatibus sed aperiam! Accusantium eos reiciendis perspiciatis!</p>
+                </div>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+    <div class="row mt-2">
+        <div class="col"></div>
+        <div class="col-8 col-lg-8 col-md-10 col-sm-10">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod praesentium esse
+                        soluta expedita recusandae dolor maxime, ab repudiandae sint quo similique quisquam! Culpa
+                        voluptatibus sed aperiam! Accusantium eos reiciendis perspiciatis!</p>
+                </div>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+    <div class="row mt-2">
+        <div class="col"></div>
+        <div class="col-8 col-lg-8 col-md-10 col-sm-10">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod praesentium esse
+                        soluta expedita recusandae dolor maxime, ab repudiandae sint quo similique quisquam! Culpa
+                        voluptatibus sed aperiam! Accusantium eos reiciendis perspiciatis!</p>
+                </div>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+    <div class="row mt-2">
+        <div class="col"></div>
+        <div class="col-8 col-lg-8 col-md-10 col-sm-10">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod praesentium esse
+                        soluta expedita recusandae dolor maxime, ab repudiandae sint quo similique quisquam! Culpa
+                        voluptatibus sed aperiam! Accusantium eos reiciendis perspiciatis!</p>
+                </div>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+    <div class="row mt-2">
+        <div class="col"></div>
+        <div class="col-8 col-lg-8 col-md-10 col-sm-10">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod praesentium esse
+                        soluta expedita recusandae dolor maxime, ab repudiandae sint quo similique quisquam! Culpa
+                        voluptatibus sed aperiam! Accusantium eos reiciendis perspiciatis!</p>
+                </div>
+            </div>
+        </div>
+        <div class="col"></div>
     </div>
 </template>
 
@@ -8,6 +95,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
